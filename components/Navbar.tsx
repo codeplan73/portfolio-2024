a58a6cad7d05d2b71498d87cbd57c720209d1e71
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-11-12 23:09:29 +0100

    build navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { routes } from "@/utils/contant";
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+  return (
+    <nav className="shadow drop-shadow-2xl">
+      <div className="flex items-center justify-between px-6 py-4 mx-auto md:container md:px-12">
+        <Image
+          src="/logo.png"
+          className="w-12 h-12 rounded-full"
+          width={1000}
+          height={1000}
+          alt="logo"
+        />
+        <div className="flex items-center space-x-6">
+          <ul className="flex items-center hidden space-x-6 md:flex">
+            {routes.map((route) => (
+              <li key={route.name}>
+                <Link
+                  className="poppins-regular hover:text-primaryColor easse-in-out text-md"
+                  href={route.path}
+                >
+                  {route.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <Button className="px-4 py-2 text-white duration-300 rounded-full shadow hover:drop-shadow-xl bg-primaryColor poppins-bold hover:bg-primaryColor/80">
+            Hire Me
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ utils/contant.ts
@@ -0,0 +1,26 @@
+export const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About Me",
+    path: "/#about",
+  },
+  {
+    name: "Services",
+    path: "/#servcies",
+  },
+  {
+    name: "Projects",
+    path: "/#projects",
+  },
+  {
+    name: "Testimonials",
+    path: "/#testimonials",
+  },
+  {
+    name: "Contact",
+    path: "/#contact",
+  },
+];
