1f0983704597bf8b6cc647102dc591541168abe3
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-11-23 18:35:44 +0100

    installed packages

---
+++ next.config.js
@@ -0,0 +1,31 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "avatar.vercel.sh",
+//         port: "",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatar.vercel.sh",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
